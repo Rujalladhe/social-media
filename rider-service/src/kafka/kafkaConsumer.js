4bd6aa476148dd0662ab7cece21f8eca1ba0f15f
rujaltladhe <rujal.ladhe21@gmail.com>
2025-11-11 07:57:11 +0530

    fix: correct typo in Kafka subscription log message

--- rider-service/src/kafka/kafkaConsumer.js
+++ rider-service/src/kafka/kafkaConsumer.js
@@ -65,7 +65,7 @@ const run = async () => {
   logger.log('[Kafka] Consumer connected');
 
   await consumer.subscribe({ topic: 'rider-locations', fromBeginning: false });
-  logger.log('[Kafka] Subscribed to rider-locations topic');
+  logger.log('[Kafka] Subscribed to rider-locationss topic');
 
   await consumer.run({
     eachMessage: async ({ topic, partition, message }) => {
